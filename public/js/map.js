db73159125cd865c756ab86e91e6df97ae7ccbf8
Apratap123 <anujrajput555.nr@gmail.com>
2024-07-20 11:07:30 +0530

    fixing map

--- public/js/map.js
+++ public/js/map.js
@@ -1,18 +1,4 @@
 
   mapboxgl.accessToken = mapToken; 
   
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v12',
-        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9 // starting zoom
-    });
-
-
-  
-
-    const marker = new mapboxgl.Marker({color:'red'})
-        .setLngLat(listing.geometry.coordinates)  //Listing.geometry.coordinates
-        .setPopup( new mapboxgl.Popup({offset: 25})
-        .setHTML(`${listing.location}<h4></h4><p>Exact Location provided after booking</p>`))
-        .addTo(map);
+ 
